2dbe1ac1cf8e66e0e8086979565848baadf02907
Amir Farmani <afarmani1a@gmail.com>
2021-10-13 01:38:15 -0400

    added base function for lambda

--- endless-projects-stock-checker-function/src/main/java/com/endless/projects/stock/checker/lambda/StockCheckerFunction.java
+++ endless-projects-stock-checker-function/src/main/java/com/endless/projects/stock/checker/lambda/StockCheckerFunction.java
@@ -1,38 +1,28 @@
 package com.endless.projects.stock.checker.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Map;
 
-public class StockCheckerFunction implements RequestHandler {
+public class StockCheckerFunction extends BaseFunction {
 
-    private static final String URL_TO_CHECK = "https://www.bestbuy.ca/en-ca/category/graphics-cards/20397";
-
-    ObjectMapper objectMapper = new ObjectMapper();
-    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private static final String URL_TO_CHECK = "https://www.bestbuy.ca/ecomm-api/availability/products?accept=application%2Fvnd.bestbuy.simpleproduct.v1%2Bjson&accept-language=en-CA&locations=910%7C544%7C795%7C916%7C954%7C207%7C926%7C622%7C932%7C57%7C200%7C202%7C938%7C233%7C927%7C617%7C990%7C930%7C237%7C203%7C62%7C956%7C977%7C931%7C245%7C615%7C965%7C937%7C943%7C985%7C949%7C942%7C631&postalCode=L6R1Z8&skus=15463567%7C15166285%7C15078017%7C15530046%7C15463568%7C15530045%7C14969729%7C15318940%7C14954116%7C15229237%7C10416248%7C15309513%7C15084753%7C15546964%7C15147122%7C15309514%7C15373182%7C15524485%7C15493494%7C14954117%7C15317226%7C15547752%7C15201199%7C15201201";
 
     @Override
-    public Object handleRequest(Object input, Context context) {
-        LambdaLogger logger = context.getLogger();
-        logger.log("CONTEXT: " + gson.toJson(context));
-
+    public String process(Map<String, String> event, Context context) {
         InputStream response = null;
         try {
             URLConnection urlConnection = new URL(URL_TO_CHECK).openConnection();
+            urlConnection.setRequestProperty("user-agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36");
             response = urlConnection.getInputStream();
+            System.out.println(response);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        System.out.println("StockCheckerFunction");
-        return null;
+        return "success";
     }
 }
--- endless-projects-stock-checker-function/src/test/java/com/endless/projects/stock/checker/lambda/StockCheckerFunctionTest.java
+++ endless-projects-stock-checker-function/src/test/java/com/endless/projects/stock/checker/lambda/StockCheckerFunctionTest.java
@@ -1,7 +1,27 @@
 package com.endless.projects.stock.checker.lambda;
 
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class StockCheckerFunctionTest  {
 
+    private static final String URL_TO_CHECK = "https://www.bestbuy.ca/ecomm-api/availability/products?accept=application%2Fvnd.bestbuy.simpleproduct.v1%2Bjson&accept-language=en-CA&locations=910%7C544%7C795%7C916%7C954%7C207%7C926%7C622%7C932%7C57%7C200%7C202%7C938%7C233%7C927%7C617%7C990%7C930%7C237%7C203%7C62%7C956%7C977%7C931%7C245%7C615%7C965%7C937%7C943%7C985%7C949%7C942%7C631&postalCode=L6R1Z8&skus=15463567%7C15166285%7C15078017%7C15530046%7C15463568%7C15530045%7C14969729%7C15318940%7C14954116%7C15229237%7C10416248%7C15309513%7C15084753%7C15546964%7C15147122%7C15309514%7C15373182%7C15524485%7C15493494%7C14954117%7C15317226%7C15547752%7C15201199%7C15201201";
 
+    @Test
+    private void UrlConnectionTest() throws IOException {
+        URLConnection urlConnection = new URL(URL_TO_CHECK).openConnection();
+        urlConnection.setRequestProperty("user-agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36");
+        InputStream response = urlConnection.getInputStream();
+
+        Assert.assertNotNull(response);
+
+
+    }
 }
